7c30c73ecd48d0feee36336a45e683bcdaa22a76
Mario Reder <mario.reder@pm.me>
2025-10-09 09:05:40 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "Block",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/block-8538/",
+  "VITE_SEO_SITE_URL": "https://dex.blockchainworks.org",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@Blockdex",
